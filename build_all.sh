8e2bcb0430b8bb05d750d5d48cb15957f854b1ee
Yash Jetwani <yjetwani@vmware.com>
2019-03-02 12:44:06 +0530

    switched on build flag

--- build_all.sh
+++ build_all.sh
@@ -2,5 +2,5 @@
 
 curl https://download.samba.org/pub/samba/stable/samba-4.9.4.tar.gz > samba.tar.gz
 tar -xvf samba.tar.gz && mv samba-4.9.4 samba
-./build_debug.sh 0
-./build_rel.sh 0
+./build_debug.sh 1
+./build_rel.sh 1
